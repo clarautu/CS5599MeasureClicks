80e194c24259b4847d63ae0321aa931a70d0d85b
Autumn Clark <clarautu@isu.edu>
2023-10-23 12:29:37 -0600

    Added mouse click event and time and position measurements

--- gui.py
+++ gui.py
@@ -10,26 +10,29 @@ if PYQT_VER == 'PYQT5':
     from PyQt5.QtWidgets import *
     from PyQt5.QtGui import *
     from PyQt5.QtCore import *
-    from PyQt5.QtTest import QTest
 else:
     raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
-class gui:
+class gui(QWidget):
     def __init__(self):
+        super().__init__()
+        self.setMouseTracking(True)
+        self.startTime = -1
+        self.counter = 0
+        self.trials = 5
+        self.testing = False
         self.rand = random
         self.rows = 8
         self.columns = 15
         self.currentMode = ""
-        self.window = QWidget()
         self.SetUp()
         self.switchMode()
         self.Show()
 
     # Method that performs initial setup for the window
     def SetUp(self):
-        self.window = QWidget()
-        self.window.setWindowTitle("Measure Click Accuracy and Speed")
+        self.setWindowTitle("Measure Click Accuracy and Speed")
 
         # Create label that shows what mode is currently selected
         lbCurrentMode = QLabel("Current Mode: " + self.currentMode)
@@ -68,32 +71,32 @@ class gui:
         mainLayout.addLayout(btnBar)
         mainLayout.addLayout(testArea)
 
-        self.window.setLayout(mainLayout)
+        self.setLayout(mainLayout)
 
     # Method that shows the window
     def Show(self):
-        self.window.showMaximized()
+        self.showMaximized()
 
     # Click event that switches between light and dark modes
     def switchMode(self):
         if self.currentMode == "Light Mode":
             self.currentMode = "Dark Mode"
             self.updateLabel()
-            self.window.setStyleSheet("background-color: rgb(62,62,66); color: white")
-            for child in self.window.children():
+            self.setStyleSheet("background-color: rgb(62,62,66); color: white")
+            for child in self.children():
                 if type(child) == QPushButton:
                     child.setStyleSheet("background-color: rgb(37,37,38)")
         else:
             self.currentMode = "Light Mode"
             self.updateLabel()
-            self.window.setStyleSheet("background-color: white; color: black")
-            for child in self.window.children():
+            self.setStyleSheet("background-color: white; color: black")
+            for child in self.children():
                 if type(child) == QPushButton:
                     child.setStyleSheet("background-color: lightGrey")
 
     # Method that updates the current mode label
     def updateLabel(self):
-        for child in self.window.children():
+        for child in self.children():
             if type(child) == QLabel:
                 if child.text() != "":
                     child.setText("Current Mode: " + self.currentMode)
@@ -103,41 +106,37 @@ class gui:
 
     # Click event that starts the test
     def start(self):
+        # Allow click tracking
+        self.testing = True
+
         # Assign the circle image as a pixmap - circle color is rgb(0,128,0)
         pixmap = QPixmap('alteredCircle.png')
 
-        # Run 5 trials
-        trialCount = 0
-        while trialCount < 5:
-            trialCount += 1  # Increment trial count
-            # Set target as a random integer on the range of zero to rows * columns
-            target = self.rand.randint(0, (self.rows * self.columns))
-            count = 0  # Track current index
+        # Set target as a random integer on the range of zero to rows * columns
+        target = self.rand.randint(0, (self.rows * self.columns))
+        count = 0  # Track current index
 
-            # Loop through children and hide buttons and status label
-            for child in self.window.children():
-                if type(child) == QPushButton:
-                    child.hide()
-                elif type(child) == QLabel:
-                    if child.text() == "":
-                        if count == target:  # Set pixmap to target label
-                            child.setPixmap(pixmap)
-                            child.setScaledContents(True)
-                            break
-                        else:
-                            count += 1
+        # Loop through children and hide buttons and status label
+        for child in self.children():
+            if type(child) == QPushButton:
+                child.hide()
+            elif type(child) == QLabel:
+                if child.text() == "":
+                    if count == target:  # Set pixmap to target label
+                        child.setPixmap(pixmap)
+                        child.setScaledContents(True)
+                        break
                     else:
-                        child.hide()
-            self.window.repaint()  # Update window
-            # Set up timer and send timeout signal to endTest for now
-            QTest.qWait(3000)
-            self.endTest()
-            #timer = QTimer()
-            #timer.singleShot(3000, self.endTest)
+                        count += 1
+                else:
+                    child.hide()
+        self.repaint()  # Update window
+        self.startTime = time.time()
 
     # Method that ends the test
     def endTest(self):
-        for child in self.window.children():
+        self.testing = False
+        for child in self.children():
             if type(child) == QPushButton:
                 child.show()
             elif type(child) == QLabel:
@@ -145,7 +144,27 @@ class gui:
                     child.clear()
                 else:
                     child.show()
-        self.window.repaint()
+        self.repaint()
+
+    # Mouse click event - measures time from circle displaying to user click and click position
+    def mousePressEvent(self, event):
+        # If not testing, don't measure click
+        if not self.testing:
+            return
+
+        timeToClick = time.time() - self.startTime
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Time to click: " + str(timeToClick) +"\nMouse coords: ( %d : %d )" % (event.x(), event.y()))
+        msg.setWindowTitle("Click Measured")
+        msg.exec_()
+        self.counter += 1
+        if self.counter == self.trials:
+            self.counter = 0
+            self.endTest()
+        else:
+            self.endTest()
+            self.start()
 
 
 if __name__ == '__main__':
